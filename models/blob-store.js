67f2e2b8f84547e2dc588ae05b2e5b3f2df57cd8
Dpazdniakova <20107671@setu.ie>
2026-02-02 22:00:24 +0000

    blob

--- models/blob-store.js
+++ models/blob-store.js
@@ -6,67 +6,76 @@
 import { getStore } from '@netlify/blobs'
 
 class BlobStore {
-  constructor(file, defaults) {
-    this.db = getStore(file, defaults);
-    this.db.read();
+  constructor(storeName, defaults) {
+    this.storeName = storeName;
+    this.defaults = defaults;
+    
+    // Use getStore with just the name - it should auto-detect the environment
+    try {
+      this.store = getStore(storeName);
+    } catch (error) {
+      console.error('Failed to initialize blob store:', error);
+      // Fallback: initialize with empty context for now
+      this.store = null;
+    }
   }
 
   findAll(collection) {
-    return this.db.data[collection];
+    return this.store.data[collection];
   }
 
   findBy(collection, filter) {
-    const results = this.db.data[collection].filter(filter);
+    const results = this.store.data[collection].filter(filter);
     return results;
   }
 
   findOneBy(collection, filter) {
-    const results = this.db.data[collection].filter(filter);
+    const results = this.store.data[collection].filter(filter);
     return results[0];
   }
 
   async addCollection(collection, obj) {
-    this.db.data[collection].push(obj);
-    await this.db.write();
+    this.store.data[collection].push(obj);
+    await this.store.write();
   }
 
   async addItem(collection, id, arr, obj) {
-    const data = this.db.data[collection].filter((c) => c.id === id);
+    const data = this.store.data[collection].filter((c) => c.id === id);
     data[0][arr].push(obj);
-    await this.db.write();
+    await this.store.write();
   }
 
   async removeCollection(collection, obj) {
-    const index = this.db.data[collection].indexOf(obj);
+    const index = this.store.data[collection].indexOf(obj);
     if (index > -1) {
-      this.db.data[collection].splice(index, 1);
+      this.store.data[collection].splice(index, 1);
     }
-    await this.db.write();
+    await this.store.write();
   }
 
   async removeItem(collection, id, arr, itemId) {
-    const data = this.db.data[collection].filter((c) => c.id === id);
+    const data = this.store.data[collection].filter((c) => c.id === id);
     const item = data[0][arr].filter((i) => i.id === itemId);
     const index = data[0][arr].indexOf(item[0]);
     if (index > -1) {
       data[0][arr].splice(index, 1);
     }
-    await this.db.write();
+    await this.store.write();
   }
 
   async editCollection(collection, id, obj) {
-    let index = this.db.data[collection].findIndex((c) => c.id === id);
+    let index = this.store.data[collection].findIndex((c) => c.id === id);
     if (index > -1) {
-      this.db.data[collection].splice(index, 1, obj);
+      this.store.data[collection].splice(index, 1, obj);
     }
-    await this.db.write();
+    await this.store.write();
   }
 
   async editItem(collection, id, itemId, arr, obj) {
-    const data = this.db.data[collection].filter((c) => c.id === id);
+    const data = this.store.data[collection].filter((c) => c.id === id);
     let index = data[0][arr].findIndex((i) => i.id === itemId);
     data[0][arr].splice(index, 1, obj);
-    await this.db.write();
+    await this.store.write();
   }
 }
 
